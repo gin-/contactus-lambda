76ad747da0fbbb788bd3133e8ba698c3ce27968a
Andrii Hrushko <ahrushko@blackbird-lab.com>
2019-03-21 16:11:44 +0200

    added attachments support

--- app.js
+++ app.js
@@ -1,154 +1,114 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const multer = require('multer');
+const upload = multer();
 const AWS = require('aws-sdk');
+const nodemailer = require("nodemailer");
 const SES = new AWS.SES({
-    region: 'us-east-1'
+  region: 'us-east-1'
 });
 
 // declare a new express app
 const app = express();
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(upload.single('attachment'));
 
 // Enable CORS for all methods
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next()
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next()
 });
-
 
 AWS.config.update({region: process.env.REGION});
 
 app.post('/blackbird-home/contacts', function (req, res) {
-    console.log(req.body);
-    const params = req.body;
-    if (!params.email) {
-        console.log("Empty email. EMAIL WON'T BE SENT");
-        res.status(400).send("Empty email!");
-        return;
-    }
+  console.log(req.file);
+  const params = req.body;
+  if (!params.email) {
+    console.log("Empty email. EMAIL WON'T BE SENT");
+    res.status(400).send("Empty email!");
+    return;
+  }
 
-    const bbEmail = 'info@blackbird-lab.com';
+  const bbEmail = 'ahrushko@blackbird-lab.com';
 
-    const eParams = {
-        Destination: {
-            ToAddresses: [bbEmail]
-        },
-        Message: {
-            Body: {
-                Html: {
-                    Data: `<h3>New message from Contact Us on blackbird-lab.com:</h3></br></hr></br>
+  const mailOptions = {
+    from: bbEmail,
+    replyTo: params.email,
+    to: bbEmail,
+    subject: `Contact us message from ${params.firstName} ${params.lastName}`,
+    html: `<h3>New message from Contact Us on blackbird-lab.com:</h3></br></hr></br>
                             <p><b>From:</b> ${params.firstName} ${params.lastName}</p>
                             <p><b>Email:</b> ${params.email}</p>
                             <p><b>Country:</b> ${params.country}</p>
                             <p><b>Message:</b></p>
                             <p>${params.message}</p>`,
-                    Charset: "UTF8"
-                },
-                Text: {
-                    Data: `New message from Contact Us on blackbird-lab.com:\nFrom ${params.firstName} ${params.lastName}\n${params.message}\nPlease reply to email: ${params.email}`,
-                    Charset: "UTF8"
-                }
-            },
-            Subject: {
-                Data: `Contact us message from ${params.firstName} ${params.lastName}`,
-                Charset: "UTF8"
-            }
-        },
-        ReplyToAddresses: [
-            params.email
-        ],
-        Source: bbEmail
-    };
+    text: `New message from Contact Us on blackbird-lab.com:\\nFrom ${params.firstName} ${params.lastName}\\n${params.message}\\nPlease reply to email: ${params.email}`,
+    attachments: [
+      {
+        filename: req.file.originalname,
+        content: req.file.buffer
+      }
+    ]
+  };
 
-    console.log('===SENDING CONTACT US EMAIL===');
-    const email = SES.sendEmail(eParams, function (err, data) {
-        if (err) {
-            console.log(err);
-            res.status(500).json(err);
-        } else {
-            console.log("===EMAIL SENT===");
-            console.log(data);
+  const transporter = nodemailer.createTransport({
+    SES: SES
+  });
 
-
-            console.log("EMAIL CODE END");
-            console.log('EMAIL: ', email);
-            res.json({
-                isBase64Encoded: false,
-                statusCode: 200,
-                headers: {},
-                body: {message: "Email has been sent."}
-            });
-        }
-    });
+  transporter.sendMail(mailOptions, function (err, info) {
+    if (err) {
+      console.log("Error sending email");
+      res.status(500).send('Internal Error: The message is not accepted.');
+    } else {
+      console.log("Email sent successfully");
+      res.sendStatus(200);
+    }
+  });
 });
 
-/*TODO: implement*/
 app.post('/blackbird/vac_apply', function (req, res) {
-    console.log(req.body);
-    res.status(400).send("Not implemented yet");
-    const params = req.body;
-    if (!params.email) {
-        console.log("Empty email. EMAIL WON'T BE SENT");
-        return;
+  if (!req.file) {
+    console.log("Empty attachment. EMAIL WON'T BE SENT");
+    res.status(400).send("Empty attachment!");
+    return;
+  }
+
+  const bbEmail = 'ahrushko@blackbird-lab.com';
+
+  const mailOptions = {
+    from: bbEmail,
+    to: bbEmail,
+    subject: `New vacation application on Blackbird Home`,
+    html: `<h3>New vacation application on Blackbird Home (see attachment)</h3>`,
+    text: `New vacation application on Blackbird Home (see attachment)`,
+    attachments: [
+      {
+        filename: req.file.originalname,
+        content: req.file.buffer
+      }
+    ]
+  };
+
+  const transporter = nodemailer.createTransport({
+    SES: SES
+  });
+
+  transporter.sendMail(mailOptions, function (err, info) {
+    if (err) {
+      console.log("Error sending email");
+      res.status(500).send('Internal Error: The message is not accepted.');
+    } else {
+      console.log("Email sent successfully");
+      res.sendStatus(200);
     }
-
-    const bbEmail = 'info@blackbird-lab.com';
-
-    const eParams = {
-        Destination: {
-            ToAddresses: [bbEmail]
-        },
-        Message: {
-            Body: {
-                Html: {
-                    Data: `<h3>New subscription from on blackbird-lab.com:</h3></br></hr></br>
-                            <p><b>Email:</b> ${params.email}</p>`,
-                    Charset: "UTF8"
-                },
-                Text: {
-                    Data: `New subscription on blackbird-lab.com:\nEmail: ${params.email}`,
-                    Charset: "UTF8"
-                }
-            },
-            Subject: {
-                Data: `Subscription message from ${params.email}`,
-                Charset: "UTF8"
-            }
-        },
-        ReplyToAddresses: [
-            params.email
-        ],
-        Source: bbEmail
-    };
-
-    console.log('===SENDING SUBSCRIPTION EMAIL===');
-    const email = SES.sendEmail(eParams, function (err, data) {
-        if (err) {
-            console.log(err);
-            res.json(err);
-        } else {
-            console.log("===EMAIL SENT===");
-            console.log(data);
-
-
-            console.log("EMAIL CODE END");
-            console.log('EMAIL: ', email);
-            res.json({
-                isBase64Encoded: false,
-                statusCode: 200,
-                headers: {},
-                body: {message: "Email has been sent."}
-            });
-        }
-    });
+  });
 });
 
 app.listen(3000, function () {
-    console.log("App started")
+  console.log("App started")
 });
 
-// Export the app object. When executing the application local this does nothing. However,
-// to port it to AWS Lambda we will create a wrapper around that will load the app from
-// this file
 module.exports = app;
